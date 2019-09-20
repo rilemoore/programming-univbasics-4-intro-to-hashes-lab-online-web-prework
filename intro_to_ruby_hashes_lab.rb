b025c5e2ba204ab3276ecda5820d0f76bdcd6f2e
Riley Moore <rilemoore@gmail.com>
2019-09-20 12:28:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,8 @@ def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
 end
 
-def read_from_halearnsh(hash, key)
+def read_from_hash(hash, key)
+  return 
   # return the correct value using the hash and key parameters
 end
 
